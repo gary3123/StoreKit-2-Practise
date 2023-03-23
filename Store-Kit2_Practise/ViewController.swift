b3e3c509b5c5274c7e2e61a24d3633fbea5ecd19
Gary <ban103123@gmail.com>
2023-03-23 14:45:20 +0800

    Initial Commit

---
+++ Store-Kit2_Practise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Store-Kit2_Practise
+//
+//  Created by Gary on 2023/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
